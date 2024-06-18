04314a595ea62e2444e788b6aa65c0d18dda59f2
Pathak.S <76900280+s-pathak-tech@users.noreply.github.com>
2024-06-18 20:49:28 +0530

    Update streamlit_app.py
    Adding DataFrame Only

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
