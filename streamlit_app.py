7f4e8c199bd1945672391448972ebe37e653b85b
Pathak.S <76900280+s-pathak-tech@users.noreply.github.com>
2024-06-18 20:48:13 +0530

    Update streamlit_app.py
    Remove dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ name_on_order = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose UpTo 5 Ingredients",
